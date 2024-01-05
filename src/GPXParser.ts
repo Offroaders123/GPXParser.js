7fe9b66372b14501c12d3c67ffa482d78f8335d8
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2024-01-05 03:44:51 -0800

    Types Embedding
    Moved the library definitions into the types of the source file now, as well.

--- src/GPXParser.ts
+++ src/GPXParser.ts
@@ -1,5 +1,3 @@
-import type { MetaData, Waypoint, Track, Route, Author, Link, Point, Distance, Elevation } from "../index.js";
-
 /**
  * GPX file parser
  */
@@ -457,4 +455,88 @@ export default class GPXParser {
 
 if (typeof module !== 'undefined') {
   require('jsdom-global')();
+}
+
+export interface MetaData {
+  name: string;
+  desc: string;
+  link: Link;
+  author: Author;
+  time: Date;
+}
+
+export interface Waypoint {
+  name: string;
+  sym: string;
+  cmt: string;
+  desc: string;
+  lat: number;
+  lon: number;
+  ele: number | null;
+  time: Date | null;
+}
+
+export interface Track {
+  name: string;
+  cmt: string;
+  desc: string;
+  src: string;
+  number: string;
+  link: Link;
+  type: string | null;
+  points: Point[];
+  distance: Distance;
+  elevation: Elevation;
+  slopes: number[];
+}
+
+export interface Route {
+  name: string;
+  cmt: string;
+  desc: string;
+  src: string;
+  number: string;
+  link: Link;
+  type: string | null;
+  points: Point[];
+  distance: Distance;
+  elevation: Elevation;
+  slopes: number[];
+}
+
+export interface Point {
+  lat: number;
+  lon: number;
+  ele: number | null;
+  time: Date | null;
+}
+
+export interface Distance {
+  total: number;
+  cumul: number[];
+}
+
+export interface Elevation {
+  max: number | null;
+  min: number | null;
+  pos: number | null;
+  neg: number | null;
+  avg: number | null;
+}
+
+export interface Author {
+  name: string;
+  email: Email;
+  link: Link;
+}
+
+export interface Email {
+  id: string;
+  domain: string;
+}
+
+export interface Link {
+  href: string;
+  text: string;
+  type: string;
 }
